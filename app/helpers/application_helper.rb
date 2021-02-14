095788ed4fae88bbea22173155023cf53627719d
Eliot Piering <eliot@eliotpiering.com>
2021-02-13 23:31:57 -0600

    bunch of small fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,17 @@
 module ApplicationHelper
-  def navbar_item(name, path, options={})
+  def navbar_item(name, path, options = {})
     classes = "nav-link "
     classes += "active " if current_page?(path)
 
-
     content_tag(:li, class: "nav-item") do
-      content_tag(:a, name, class: classes, aria_current: "page", href: path)
+      content_tag(:a, name, class: classes, aria_current: "page", href: path, **options)
     end
   end
 
   def bi_icon(icon, options = {})
     klasses = ["bi"].append(options.delete(:class)).compact
     content_tag :svg, options.merge(class: klasses, fill: "currentColor") do
-      content_tag :use, nil, "xlink:href" => "#{asset_path 'bootstrap-icons/bootstrap-icons.svg'}##{icon}"
+      content_tag :use, nil, "xlink:href" => "#{asset_path "bootstrap-icons/bootstrap-icons.svg"}##{icon}"
     end
   end
 end
