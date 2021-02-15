8888662226844c3aab6e2c159b407452fa3115d6
Eliot Piering <eliot@eliotpiering.com>
2021-02-15 13:38:15 -0600

    clean up images; about img; fix mail domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: Rails.application.credentials[:mailgun_sending_key],
-    domain: "none"
+    domain: "mail.rrealfitness.com",
   }
-  # domain: "mail.rrealfitness.com",
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
